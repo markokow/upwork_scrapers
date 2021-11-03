1cfd42c4318e66c5597b8b2e845c8f32c046a315
Rex Von Brixon Aparece Apa-ap <rex.von_brixon_apa-ap@bp.com>
2021-11-03 14:00:26 +0800

    updated ig scrape

--- Upwork/instagram_scrape/instagram_scrape.py
+++ Upwork/instagram_scrape/instagram_scrape.py
@@ -1,15 +1,15 @@
 import os
 import json
-import pandas as pd
 from datetime import datetime
 import csv
-
-result: list = []
+import os.path
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
 
 # os.system("pip install instagram-scraper")
-account: str = 'kevin'
+account: str = 'e.r.pods'
 
-# os.system(f"instagram-scraper {account} -u rexvon.apaap@gmail.com -p Bryitkids92073 --media-metadata --include-location --profile-metadata -m 10")
+os.system(f"instagram-scraper {account} -u rexvon.apaap@gmail.com -p Bryitkids92073 --media-metadata --include-location --profile-metadata -m 10")
 
 file = open(f'{account}/{account}.json', encoding='utf-8')
 
@@ -17,10 +17,8 @@ data = json.load(file)
 
 posts = data['GraphImages']
 
-
+result: list = []
 result.append(['post_date','media_url','caption','comment_count','number_of_likes'])
-
-# counter = 1
 
 for dat in posts:
 
@@ -32,16 +30,6 @@ for dat in posts:
     _caption = dat['edge_media_to_caption']['edges'][0]['node']['text']
 
     result.append([_post_date,_media_url,_caption,_comment_count,_number_of_likes])
-
-    # _features = {
-    #     'post_date': _post_date,
-    #     'media_url': _media_url,
-    #     'caption': _caption,
-    #     'comment count': _comment_count,
-    #     'number of likes': _number_of_likes,
-    # }
-
-    # result.append(_features)
 
 def _write_csv(result: list = []):
     '''Save results to csv.'''
@@ -59,17 +47,6 @@ def _write_csv(result: list = []):
     else:
         print('No data was saved')
         
-
-# _write_csv(result = result)
-
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-
-from google.oauth2 import service_account
-
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SERVICE_ACCOUNT_FILE = 'intense-talent-327208-2edab6a7d6ae.json'
@@ -87,8 +64,8 @@ service = build('sheets', 'v4', credentials=credentials)
 # Call the Sheets API
 sheet = service.spreadsheets()
 
-test = [["hey", "hi"],[1,2]]
-
 result = service.spreadsheets().values().update(
     spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME,
     valueInputOption="USER_ENTERED", body={"values": result}).execute()
+
+print(result)
