cad584fdcc4dde40c9fd77de485cb6b794676562
Rex Von Brixon Aparece Apa-ap <rex.von_brixon_apa-ap@bp.com>
2021-11-04 11:10:31 +0800

    updated

--- Upwork/instagram_scrape/instagram_scrape.py
+++ Upwork/instagram_scrape/instagram_scrape.py
@@ -1,15 +1,16 @@
 import os
 import json
 from datetime import datetime
-import csv
 import os.path
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
+import shutil
 
 # os.system("pip install instagram-scraper")
 account: str = 'fritzhansen'
 
-os.system(f"instagram-scraper {account} -u rexvon.apaap@gmail.com -p Bryitkids92073 --media-metadata --include-location --profile-metadata -m 20")
+
+os.system(f"instagram-scraper {account} -u rexvon.apaap@gmail.com -p Bryitkids92073 --media-metadata --include-location --profile-metadata -m 1")
 
 file = open(f'{account}/{account}.json', encoding='utf-8')
 
@@ -30,42 +31,30 @@ for dat in posts:
     _caption = dat['edge_media_to_caption']['edges'][0]['node']['text']
 
     result.append([_post_date,_media_url,_caption,_comment_count,_number_of_likes])
-
-def _write_csv(result: list = []):
-    '''Save results to csv.'''
-    print('Saving to csv....')
-
-    if result: 
-        with open(f'{account}.csv', 'w', newline = '', encoding = 'utf-8') as csv_file:
-            writer_object = csv.DictWriter(csv_file, fieldnames = result[0].keys())
-            writer_object.writeheader()
-
-            for row in result:
-                writer_object.writerow(row)
-        print("Saved to results.xlsx")
-    
-    else:
-        print('No data was saved')
         
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SERVICE_ACCOUNT_FILE = 'intense-talent-327208-2edab6a7d6ae.json'
+SERVICE_ACCOUNT_FILE = 'dev-monolith-331102-d5754db1b0f8.json'
 
 credentials = None
 credentials = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1nciStQYv5ryCxrkKx5APFWOmJk1_MMudynGE6VJTzg4'
-SAMPLE_RANGE_NAME = 'data!A1'
+SAMPLE_SPREADSHEET_ID = '17kopo5eo2-Xmw27yItjvv3J3S-V0CwzA2hseqy_LUPc'
+SAMPLE_RANGE_NAME = 'IG1!A1'
 
 service = build('sheets', 'v4', credentials=credentials)
 
 # Call the Sheets API
 sheet = service.spreadsheets()
 
-# result = service.spreadsheets().values().update(
-#     spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME,
-#     valueInputOption="USER_ENTERED", body={"values": result}).execute()
+result = service.spreadsheets().values().update(
+    spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME,
+    valueInputOption="USER_ENTERED", body={"values": result}).execute()
 
-# print(result)
+print(result)
+
+shutil.rmtree(f'{account}')
+print("Successfully deleted folder and all of its contents.")
+
